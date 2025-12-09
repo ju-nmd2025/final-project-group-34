3befe3a9cb1b8d72ac7ef793b9dc69b285ef6bc4
Lily <yazi25ur@student.ju.se>
2025-12-09 19:09:20 +0100

    Update doodler.js

--- doodler.js
+++ doodler.js
@@ -1,2 +1,11 @@
-Hello
-hi
+class Doodler {
+    constuctor(){
+        this.r = 50;
+        this.x = width/2;
+        this.y = height-200;
+        this.vy = 0;
+        this.gravity = 0.4;
+        this.jumpForce = -10;
+    }
+}
+
