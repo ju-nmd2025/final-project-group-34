36ff333a1469c341f40c25ac1b75b4f29d62adf0
ANDREW8118888 <13021974166@163.com>
2025-12-10 00:07:43 +0100

    Update platform.js

--- platform.js
+++ platform.js
@@ -1,2 +1,26 @@
-hello
-hello
+class Platform {
+    constructor(x,y,type){
+        this.x = x;
+        this.y = y;
+        this.w=70;
+        this.h=15;
+        this.type = type;
+        this.isBroken = false;
+        this.Direction = 1;
+        this.speed = 2;
+    }
+
+    update(){
+            if (this.type === 1){
+                this.x += this.speed * this.Direction;
+                if (this.x + this.w/2 > width || this.x - this.w/2 < 0){
+                    this.Direction *= -1;
+                }
+            }
+        }
+
+            show(){
+                if (this.isBroken) return;
+                rect(this.x, this.y, this.w, this.h);
+            }
+        }
