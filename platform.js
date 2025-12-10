d7524b8ae75a22486de37553416badd0740e0ad9
ANDREW8118888 <13021974166@163.com>
2025-12-10 21:14:43 +0100

    Update platform.js

--- platform.js
+++ platform.js
@@ -21,6 +21,24 @@ class Platform {
 
             show(){
                 if (this.isBroken) return;
-                rect(this.x, this.y, this.w, this.h);
-            }
-        }
+
+                noStroke();
+                rectMode(CENTER);
+                if (this.type === 0){
+                    fill(100,200,100);
+                    rect(this.x,this.y,this.w,this.h,10);
+                }
+                else if (this.type === 1){
+                    fill(200,230,255);
+                    rect(this.x,this.y,this.w,this.h,10);
+                }
+                else if (this.type === 2){
+                    fill(200,100,50);
+                    rect(this.x,this.y,this.w,this.h,5);
+                    stroke(100,50,0);
+                    strokeWeight(2);
+                   line(this,x-10,this.y-5,this.x+10,this.y+5);
+
+                }
+            } 
+    }
