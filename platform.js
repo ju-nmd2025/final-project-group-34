a7ca6cbaca43bbeea44bf50c82169871b4cc8850
ANDREW8118888 <13021974166@163.com>
2025-12-13 01:11:35 +0100

    Update platform.js

--- platform.js
+++ platform.js
@@ -1,48 +1,34 @@
 class Platform {
-    constructor(x,y,type){
-        this.x = x;
-        this.y = y;
-        this.w=70;
-        this.h=15;
-        this.type = type;
-        this.isBroken = false;
-        this.Direction = 1;
-        this.speed = 2;
+  constructor(x, y, type) {
+    this.x = x;
+    this.y = y;
+    this.w = 70;
+    this.h = 15;
+    this.type = type; 
+    this.isBroken = false;
+    
+    this.dir = 1; 
+    this.speed = 2;
+  }
+
+  update() {
+   
+    if (this.type === 1) {
+      this.x += this.speed * this.dir;
+      if (this.x > width || this.x < 0) this.dir *= -1;
     }
+  }
 
-    update() {
-            if (this.type === 1){
-                this.x += this.speed * this.Direction;
-                if (this.x + this.w/2 > width || this.x - this.w/2 < 0){
-                    this.Direction *= -1;
-                }
-            }
-        }
+  show() {
+    if (this.isBroken) return; 
 
-            show(){
-                if (this.isBroken) return;
+    rectMode(CENTER);
+    noStroke();
 
-                noStroke();
-                rectMode(CENTER);
-                if (this.type === 0){
-                    fill(100,200,100);
-                    rect(this.x,this.y,this.w,this.h,10);
-                }
-                else if (this.type === 1){
-                    fill(200,230,255);
-                    rect(this.x,this.y,this.w,this.h,10);
-                    ellipse(this.x - 20, this.y, 30, 20);
-                    ellipse(this.x + 20, this.y, 30, 20);
-                    ellipse(this.x, this.y - 10, 40, 30);
-                    
-                }
-                else if (this.type === 2){
-                    fill(200,100,50);
-                    rect(this.x,this.y,this.w,this.h,5);
-                    stroke(100,50,0);
-                    strokeWeight(2);
-                   line(this,x-10,this.y-5,this.x+10,this.y+5);
+    if (this.type === 0) fill(100, 255, 100); 
+    else if (this.type === 1) fill(100, 200, 255); 
+    else if (this.type === 2) fill(255, 100, 100); 
 
-                }
-            } 
-     } 
+    rect(this.x, this.y, this.w, this.h, 5);
+  }
+}
