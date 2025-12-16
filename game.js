b927d3616e06b003ce9b4a0b2dc69103ba490113
Lily <yazi25ur@student.ju.se>
2025-12-16 22:00:01 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,12 @@
+<<<<<<< Updated upstream
 alert("game.js loaded on GitHub Pages");
 import { Platform } from "platform.js";
 import { Character } from "./Doodler.js";
+=======
+import { Platform } from "./platform.js";
+import { Character } from "./character.js";
+
+>>>>>>> Stashed changes
 let player;
 let platforms = [];
 let score = 0;
