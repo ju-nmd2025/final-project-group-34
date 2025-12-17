407363451a8d728290659ac73075d66bcc5ba6e3
ANDREW8118888 <shha24wf@student.ju.se>
2025-12-17 16:22:41 +0100

    Update Doodler.js

--- Doodler.js
+++ Doodler.js
@@ -1,4 +1,4 @@
- class doodler {
+ class Doodler {
   constructor() {
     this.size = 50;
     this.x = width / 2;
@@ -9,14 +9,14 @@
   }
 
   update() {
-    this.vy += this.gravity; 
+    this.vy += this.gravity; // gravity
     this.y += this.vy;
 
-    
+    // left/right movement
     if (keyIsDown(LEFT_ARROW) || keyIsDown(65)) this.x -= 5;
     if (keyIsDown(RIGHT_ARROW) || keyIsDown(68)) this.x += 5;
 
-   
+    // wall
     if (this.x > width) this.x = 0;
     else if (this.x < 0) this.x = width;
   }
@@ -25,10 +25,10 @@
     push();
     translate(this.x, this.y);
     
-   
-    fill(255, 220, 0); 
+    // sunflower 
+    fill(255, 220, 0); // yellow petals
     noStroke();
-   
+    
     for (let i = 0; i < 10; i++) {
       ellipse(0, -20, 15, 30);
       rotate(PI / 5);
@@ -37,9 +37,10 @@
     fill(100, 50, 0); 
     ellipse(0, 0, 35, 35);
     
-    fill(255); ellipse(-8, -5, 8, 8); ellipse(8, -5, 8, 8); 
-    fill(0);   ellipse(-8, -5, 3, 3); ellipse(8, -5, 3, 3); 
-    noFill(); stroke(255); strokeWeight(2); arc(0, 5, 10, 8, 0, PI); 
+    // smile
+    fill(255); ellipse(-8, -5, 8, 8); ellipse(8, -5, 8, 8); // eye
+    fill(0);   ellipse(-8, -5, 3, 3); ellipse(8, -5, 3, 3); // eye
+    noFill(); stroke(255); strokeWeight(2); arc(0, 5, 10, 8, 0, PI); // mouse
     
     pop();
   }
